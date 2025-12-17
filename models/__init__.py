f97ce69c70461c35842105c8e532d0933733977f
Elbaresiman <ulm0110201466@gmail.com>
2025-12-17 14:54:34 +0300

    Add new files

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from users.models import User
+from documents.models import Document
+from organization.models import Organization
+from departments.models import Department
+from roles.models import Role
+from signatures.models import Signature
